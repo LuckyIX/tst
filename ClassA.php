c0de23d46b7ac64e23d7d20bb1756c470c535b28
Reff <nulliscool@outlook.com>
2020-07-07 17:11:59 +0300

    setId created

--- ClassA.php
+++ ClassA.php
@@ -12,6 +12,12 @@ class A
      */
     private $name;
 
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
     public function setName(string $name): self
     {
         $this->name = $name;
